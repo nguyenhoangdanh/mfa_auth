a77c2417e25a448149d559d1bf29ec98c688e52c
nguyenhoangdanh.io <hoangdanh54317@gmail.com>
2025-02-28 08:41:42 +0700

    fix asyncHandler in middleware

--- src/middlewares/asyncHandler.ts
+++ src/middlewares/asyncHandler.ts
@@ -1,24 +1,17 @@
-import { NextFunction, Request, Response } from "express";
+import {NextFunction, Request, Response} from 'express';
 
+type AsyncControllerType = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => Promise<any>;
 
-// type AsynControllerType = (req: Request, res: Response, next: NextFunction) => Promise<any>;
-
-
-// export const asyncHandler = (controller: AsynControllerType): AsynControllerType => async(req, res , next) => {
-//     try {
-//         await controller(req, res, next);
-//     } catch (error) {
-//         next(error);
-//     }
-// }
-
-type AsyncControllerType = (req: Request, res: Response, next: NextFunction) => Promise<any>;
-
-export const asyncHandler = (controller: AsyncControllerType): AsyncControllerType => 
-    async (req, res, next) => {
-        try {
-            await controller(req, res, next);
-        } catch (error) {
-            next(error);
-        }
-    };
+export const asyncHandler =
+  (controller: AsyncControllerType): AsyncControllerType =>
+  async (req, res, next) => {
+    try {
+      await controller(req, res, next);
+    } catch (error) {
+      (next as NextFunction)(error);
+    }
+  };
